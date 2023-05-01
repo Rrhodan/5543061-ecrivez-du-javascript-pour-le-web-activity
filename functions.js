210ab507f605788fec6b5d23964fb87a4b635dac
*Rrhodan <rodolphe.roussel@outlook.fr>
2023-05-01 11:31:08 +0200

    create new branch

--- functions.js
+++ functions.js
@@ -5,25 +5,25 @@ const newsLetterEmail = document.getElementById('newsletter-email');
 let sentEmail = false;
 
 newsLetterButton.addEventListener('click', () => {
-  let newParagraph = document.createElement('p');
-  if (sentEmail) {
-    return;
-  }
-  const findAlert = document.getElementById('alert');
-  if (findAlert) {
-    findAlert.parentElement.removeChild(findAlert);
-  }
-  if (!newsLetterEmail.value) {
-    newParagraph.textContent = 'Veuillez saisir quelque chose !';
-    newParagraph.classList.add('alert-danger', 'w-100', 'p-2');
-    newParagraph.setAttribute('id', 'alert');
+    let newParagraph = document.createElement('p');
+    if (sentEmail) {
+        return;
+    }
+    const findAlert = document.getElementById('alert');
+    if (findAlert) {
+        findAlert.parentElement.removeChild(findAlert);
+    }
+    if (!newsLetterEmail.value) {
+        newParagraph.textContent = 'Il faut saisir quelque chose !';
+        newParagraph.classList.add('alert-danger', 'w-100', 'p-2');
+        newParagraph.setAttribute('id', 'alert');
+        newsLetterSection.appendChild(newParagraph);
+        return;
+    }
+    newParagraph.textContent = 'Adresse email envoyée !';
+    newParagraph.classList.add('alert-success', 'w-100', 'p-2');
     newsLetterSection.appendChild(newParagraph);
-    return;
-  }
-  newParagraph.textContent = 'Adresse email envoyée !';
-  newParagraph.classList.add('alert-success', 'w-100', 'p-2');
-  newsLetterSection.appendChild(newParagraph);
-  newsLetterSection.children[0].children[0].removeChild(newsLetterEmail);
-  newsLetterSection.children[0].children[0].removeChild(newsLetterButton);
-  sentEmail = true;
+    newsLetterSection.children[0].children[0].removeChild(newsLetterEmail);
+    newsLetterSection.children[0].children[0].removeChild(newsLetterButton);
+    sentEmail = true;
 });
